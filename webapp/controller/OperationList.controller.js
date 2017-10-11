236817c55ee18889267684ba7f66f697045bf83f
Guillaumeko <guillaume.kolly@gmail.com>
2017-10-11 15:24:39 +0000

    insert a live search field to the operation list. Let comment for click search event

--- webapp/controller/OperationList.controller.js
+++ webapp/controller/OperationList.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-	'sap/m/GroupHeaderListItem',
-	"banking/model/formatter"
-], function(Controller, JSONModel, GroupHeaderListItem, formatter) {
+	"sap/m/GroupHeaderListItem",
+	"banking/model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function(Controller, JSONModel, GroupHeaderListItem, formatter, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("banking.controller.OperationList", {
@@ -22,6 +24,30 @@ sap.ui.define([
 				title: formatGroup,
 				upperCase: true
 			});
+		},
+		
+		onFilterOperations : function (oEvent) {
+			
+			/* SEARCH WHEN ENTER IS PUSHED*/
+			/*
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("Name", FilterOperator.Contains, sQuery));
+			}
+			
+			var oList = this.getView().byId("OperationList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+			*/
+			
+			/* COMPLETE LIVE SEARCH */
+			var data = oEvent.getSource().getValue();
+			var oFilter = new sap.ui.model.Filter("Name", sap.ui.model.FilterOperator.Contains, data);
+			var oElement = this.getView().byId("OperationList");
+			var oBinding = oElement.getBinding("items");
+			oBinding.filter([oFilter]);
+ 
 		},
 		
 		handleDelete: function(oEvent) {
